ff2d9e9103c6d44e5a872e87d624542b09bbe953
Kyungjae-Jang <economyjang777@gmail.com>
2024-01-22 23:35:00 +0900

    feat: 로그인 검증 테스트 코드 작성

--- test/auth.service.test.ts
+++ test/auth.service.test.ts
@@ -1,7 +1,11 @@
 import {describe} from '@jest/globals';
-import {signUp} from "../src/auth/auth.service";
+import {signUp, validateUserPassword} from "../src/auth/auth.service";
 import {AppDataSource} from "../src/data-source";
 import {User} from "../src/auth/entity/User.entity";
+import {UserDto} from "../src/auth/dto/User.dto";
+import CryptoJs from "crypto-js";
+
+const secretKey = 'nodejs-express-practice';
 
 beforeAll(async () => {
     await AppDataSource.initialize();
@@ -9,47 +13,62 @@ beforeAll(async () => {
     const userRepository = AppDataSource.getRepository(User);
     const user = new User();
     user.emailId = 'economyjang777@gmail.com';
-    user.password = '12341234';
+    user.password = CryptoJs.AES.encrypt('12341234', secretKey).toString();
     user.userName = 'hello';
     await userRepository.save(user);
 });
 
 describe('회원가입 테스트', () => {
     test('사용자 이메일 아이디 형식 오류', async () => {
-        const body = {
-            emailId: 'economyjang777&google.com',
-            password: '12341234',
-            userName: 'helloworld'
-        }
-        await expect(signUp(body)).rejects.toThrow('이메일 형식이 잘못되었습니다.');
+        const userDto = new UserDto()
+            .setEmailId('economyjang777&gmail.com')
+            .setPassword('12341234')
+            .setUserName('helloworld');
+        await expect(signUp(userDto)).rejects.toThrow('이메일 형식이 잘못되었습니다.');
     });
 
     test('사용자 패스워드 형식 오류', async () => {
-        const body = {
-            emailId: 'economyjang777@google.com',
-            password: '123',
-            userName: 'helloworld'
-        }
-        await expect(signUp(body)).rejects.toThrow('비밀번호 형식이 잘못되었습니다.');
+        const userDto = new UserDto()
+            .setEmailId('economyjang777@gmail.com')
+            .setPassword('123')
+            .setUserName('helloworld');
+        await expect(signUp(userDto)).rejects.toThrow('비밀번호 형식이 잘못되었습니다.');
     });
 
     // 이메일 형식부터 검증
     test('사용자 이메일, 패스워드 형식 오류', async () => {
-        const body = {
-            emailId: 'economyjang777#google.com',
-            password: '123',
-            userName: 'helloworld'
-        }
-        await expect(signUp(body)).rejects.toThrow('이메일 형식이 잘못되었습니다.');
+        const userDto = new UserDto()
+            .setEmailId('economyjang777#gmail.com')
+            .setPassword('123')
+            .setUserName('helloworld');
+        await expect(signUp(userDto)).rejects.toThrow('이메일 형식이 잘못되었습니다.');
     });
 
     test('이미 존재하는 사용자인지 검증', async () => {
-        const body = {
-            emailId: 'economyjang777@gmail.com',
-            password: '12341234',
-            userName: 'hello'
-        };
-
-        await expect(signUp(body)).rejects.toThrow('이미 존재하는 사용자 입니다.');
+        const userDto = new UserDto()
+            .setEmailId('economyjang777@gmail.com')
+            .setPassword('12341234')
+            .setUserName('helloworld');
+        await expect(signUp(userDto)).rejects.toThrow('이미 존재하는 사용자 입니다.');
     });
-})
+});
+
+describe('로그인 테스트', () => {
+    const mockCallback = jest.fn();
+
+    test('존재하지 않은 사용자 일 때', async () => {
+        const emailId = 'economyjang123@gmail.com';
+        const password = '11111122222';
+
+        await validateUserPassword(emailId, password, mockCallback);
+        expect(mockCallback).toHaveBeenCalledWith(null, false, { message : '존재하지 않는 사용자 입니다.'})
+    });
+
+    test('패스워드가 일치하지 않을 때', async () => {
+        const emailId = 'economyjang777@gmail.com';
+        const password = '11111122222';
+
+        await validateUserPassword(emailId, password, mockCallback);
+        expect(mockCallback).toHaveBeenCalledWith(null, false, { message : '패스워드가 일치하지 않습니다.'});
+    });
+});
